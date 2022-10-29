56b93b91787d37b3e16655ff10b6acb0256166a9
danieldamian09 <humbertorivero9@gmail.com>
2022-10-29 16:34:23 -0300

    Add project liskov

---
+++ liskov/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Button } from './components/Button';
+
+function App() {
+	return (
+		<div className="App">
+      <h1>Principio de sustitución de Liskov (LSP) 👀</h1>
+      <Button color="secondary" size="medium">Button</Button>
+		</div>
+	);
+}
+
+export default App;
