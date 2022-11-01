ea9655ec773fbf06b5fc979630d897a59b5f9322
danieldamian09 <humbertorivero9@gmail.com>
2022-11-01 08:17:52 -0300

    Add detail liskov

--- liskov/src/App.tsx
+++ liskov/src/App.tsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import { Button } from './components/Button';
 
+// Si creamos un componente que sea RedButton y que herede de Button, este debemos poder cambiarlo por Button en cualquier parte de la app, respetando la sustitucion de Liskov
 function App() {
 	return (
 		<div className="App">
